41a9e0ac3c825bd893284c8547c3c6964bfdf907
FFEE_CO <42882359+fkx4-p@users.noreply.github.com>
2022-02-28 19:50:47 +0800

    Update Chapter1
    2022-2-28 Update. Chapter1 finished.

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: "完全自杀手册",
+    description: '自杀，从入门到入土',
+    base: '/',
+    repo: 'vuejs/vitepress',
+    themeConfig: {
+        // tip: "提示",
+        // warning: "注意",
+        // danger: "警告",
+        nav: [
+            { text: '首页', link: '/' },
+        ],
+        sidebar: [
+            { text: '第一章 前言', link: '/chapter-1/' }
+        ]
+    }
+}
