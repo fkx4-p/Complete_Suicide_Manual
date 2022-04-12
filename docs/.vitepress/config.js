fdb264a3187c7500bc11f5784c9af76a5326fdcf
FFEE_CO <wdnmd1635@gmail.com>
2022-04-12 08:03:34 +0800

    Init Chapter5

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -14,7 +14,8 @@ module.exports = {
             { text: '第一章 前言', link: '/chapter-1/' },
             { text: '第二章 Taking Medicines 药物', link: '/chapter-2/' },
             { text: '第三章 Hanging 上吊', link: '/chapter-3/' },
-            { text: '第四章 Leaping 跳楼', link: '/chapter-4/' }
+            { text: '第四章 Leaping 跳楼', link: '/chapter-4/' },
+            { text: '第五章 Cutting the Wrist and Carotid 割腕割喉等', link: '/chapter-5/' }
         ]
     }
 }
